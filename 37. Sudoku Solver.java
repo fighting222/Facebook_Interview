7988cbe64212b2332d3366d9cf3e981bc207b0a1
fighting222 <elsiechen9422@gmail.com>
2017-12-10 17:25:13 -0500

    Create 37. Sudoku Solver.java

---
+++ 37. Sudoku Solver.java
@@ -0,0 +1,45 @@
+/**思路：time = O（9^m) -> m represents the number of blanks to be filled in
+对每个需要填充的位置枚举1-9，对每个枚举判断是否符合所在行、列、九宫格sub-grid。如果符合则进行下一层递归。终止条件为填写完了整个棋盘。
+对于每个需要填数字的格子带入1到9，每代入一个数字都判定其是否合法，如果合法就继续下一次递归，结束时把数字设回'.'，判断新加入的数字是否合法时，只需要判定当前数字是否合法，不需要判定这个数组是否为数独数组，因为之前加进的数字都是合法的，这样可以使程序更加高效一些，
+*/
+class Solution {
+     public void solveSudoku(char[][] board) {
+        if(board == null || board.length == 0)
+            return;
+        solve(board);
+    }
+    
+    public boolean solve(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){//trial. Try 1 through 9
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c; //Put c for this cell
+                            
+                            if(solve(board))
+                                return true; //If it's the solution return true
+                            else
+                                board[i][j] = '.'; //Otherwise go back  // reset grid 
+                        }
+                    }
+                    
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if(board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' && 
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false; //check 3*3 block
+            }
+        }
+        return true;
+    }
+}
